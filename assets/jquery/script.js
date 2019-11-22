6a2b197d7d30d63621cb6371b71c107944c1ec37
Kelviny <kelvinyhenrique17@gmail.com>
2019-11-22 18:28:36 -0300

    Fazendo Responsividade da barra de navegação

--- assets/jquery/script.js
+++ assets/jquery/script.js
@@ -21,6 +21,102 @@ $(document).ready(function () {
         $(".dropdown-menu-gearexplorer").css("display", "none");
     });
 
+    $(".menu-loja").mouseover(function () {
+        $(".dropdown-menu-loja").css("display", "flex");
+    });
+
+    $(".menu-loja").mouseout(function () {
+        $(".dropdown-menu-loja").css("display", "none");
+    });
+
+    $(".menu-musica").mouseover(function () {
+        $(".dropdown-menu-spotfy").css("display", "flex");
+    });
+
+    $(".menu-musica").mouseout(function () {
+        $(".dropdown-menu-spotfy").css("display", "none");
+    });
+
+    $(".menu-video").mouseover(function () {
+        $(".dropdown-menu-vlcmediaplayer").css("display", "flex");
+    });
+
+    $(".menu-video").mouseout(function () {
+        $(".dropdown-menu-vlcmediaplayer").css("display", "none");
+    });
+
+    $(".menu-chrome").mouseover(function () {
+        $(".dropdown-menu-chrome").css("display", "flex");
+    });
+
+    $(".menu-chrome").mouseout(function () {
+        $(".dropdown-menu-chrome").css("display", "none");
+    });
+
+    $(".menu-configuracao").mouseover(function () {
+        $(".dropdown-menu-configuracao").css("display", "flex");
+    });
+
+    $(".menu-configuracao").mouseout(function () {
+        $(".dropdown-menu-configuracao").css("display", "none");
+    });
+
+    $(".menu-speaker").mouseover(function () {
+        $(".dropdown-menu-som").css("display", "flex");
+    });
+
+    $(".menu-speaker").mouseout(function () {
+        $(".dropdown-menu-som").css("display", "none");
+    });
+
+    $(".menu-ethernet").mouseover(function () {
+        $(".dropdown-menu-rede").css("display", "flex");
+    });
+
+    $(".menu-ethernet").mouseout(function () {
+        $(".dropdown-menu-rede").css("display", "none");
+    });
+
+    $(".menu-cloud").mouseover(function () {
+        $(".dropdown-menu-nuvem").css("display", "flex");
+    });
+
+    $(".menu-cloud").mouseout(function () {
+        $(".dropdown-menu-nuvem").css("display", "none");
+    });
+
+    $(".menu-search").mouseover(function () {
+        $(".dropdown-menu-pesquisa").css("display", "flex");
+    });
+
+    $(".menu-search").mouseout(function () {
+        $(".dropdown-menu-pesquisa").css("display", "none");
+    });
+
+    $(".menu-power").mouseover(function () {
+        $(".dropdown-menu-desligar").css("display", "flex");
+    });
+
+    $(".menu-power").mouseout(function () {
+        $(".dropdown-menu-desligar").css("display", "none");
+    });
+
+    $(".menu-lixeira").mouseover(function () {
+        $(".dropdown-menu-lixeira").css("display", "flex");
+    });
+
+    $(".menu-lixeira").mouseout(function () {
+        $(".dropdown-menu-lixeira").css("display", "none");
+    });
+
+    $(".menu-terminal").mouseover(function () {
+        $(".dropdown-menu-terminal").css("display", "flex");
+    });
+
+    $(".menu-terminal").mouseout(function () {
+        $(".dropdown-menu-terminal").css("display", "none");
+    });
+
 
 });
 
